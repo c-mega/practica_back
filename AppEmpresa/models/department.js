af4f0e96fa1bedd81c21d38b33fa8106913bf9b6
Cristina <cristina.menogarcia@gmail.com>
2020-09-05 17:44:47 +0200

    Petición GET de tabla departamentos funcionando!

--- AppEmpresa/models/department.js
+++ AppEmpresa/models/department.js
@@ -1,1 +1,18 @@
 const dbConfig = require('../dbConfig');
+
+//GET ALL - RECUPERO DEPARTAMENTOS
+
+const getAll = () => {
+    return new Promise((resolve, reject) => {
+        db.query('SELECT * FROM departamento', (err, rows) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(rows);
+        })
+    });
+}
+
+
+
+module.exports = { getAll };
--- AppEmpresa/routes/api/departments.js
+++ AppEmpresa/routes/api/departments.js
@@ -1,3 +1,19 @@
 const router = require('express').Router();
+const { getAll } = require('../../models/department');
+
+
+//RECUPERO LOS DEPARTAMENTOS CON GET
+
+router.get('/', async (req, res) => {
+    try {
+        const departments = await getAll();
+        res.json(departments);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+
 
 module.exports = router;
