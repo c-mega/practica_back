c28ae490394f06f6c6ca448bea929342c8a1761f
Cristina <cristina.menogarcia@gmail.com>
2020-09-05 16:53:30 +0200

    Petición POST funcionado. Creo new employees!

--- AppEmpresa/models/employee.js
+++ AppEmpresa/models/employee.js
@@ -1,5 +1,6 @@
 const dbConfig = require('../dbConfig');
 
+//GET ALL - RECUPERO
 const getAll = () => {
     return new Promise((resolve, reject) => {
         db.query('SELECT * FROM empleados', (err, rows) => {
@@ -11,4 +12,32 @@ const getAll = () => {
     });
 }
 
-module.exports = { getAll };
+
+//GET BY ID (para  operar con post, put y delete)
+const getById = (pEmployeeId) => {
+    return new Promise((resolve, reject) => {
+        db.query('select * from empleados where id = ?', [pEmployeeId], (err, rows) => {
+            if (err) reject(err);
+            if (rows.length !== 1) resolve(null);
+            resolve(rows[0]);
+        });
+    });
+}
+
+//CREATE - CREO new employee
+
+const create = ({ nombre, dni, sexo, fecha_nac, fecha_inc, salario, cargo, jefe_id }) => {
+    return new Promise((resolve, reject) => {
+        db.query('insert into empleados(nombre, dni, sexo, fecha_nac, fecha_inc, salario, cargo, jefe_id) values (?,?,?,?,?,?,?,?)',
+            [nombre, dni, sexo, fecha_nac, fecha_inc, salario, cargo, jefe_id], (err, result) => {
+                if (err) {
+                    reject(err);
+                } resolve(result);
+            }
+        )
+    });
+}
+
+
+
+module.exports = { getAll, getById, create };
--- AppEmpresa/routes/api/employees.js
+++ AppEmpresa/routes/api/employees.js
@@ -1,13 +1,31 @@
 const router = require('express').Router();
-const { getAll } = require('../../models/employee');
+const { getAll, getById, create } = require('../../models/employee');
 
 
-//Recupero con get
+//RECUPERO CON GET
 
 router.get('/', async (req, res) => {
     try {
         const employees = await getAll();
         res.json(employees);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+//CREO NEW EMPLOYEE CON POST
+
+router.post('/', async (req, res) => {
+    try {
+        req.body.fecha_inc = new Date();
+        const result = await create(req.body);
+        if (result['affectedRows'] === 1) {
+            //Recupero el new employee
+            const newEmployee = await getById(result['insertId']);
+            res.status(201).json({ success: 'A new employee has been inserted into the database', employee: newEmployee });
+        } else {
+            res.status(422).json({ error: 'A mistake appear and the new employee has not been inserted correctly' });
+        }
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
